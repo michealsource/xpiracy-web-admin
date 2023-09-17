accf5185e87138b6a24920791d49dc4b9ac76420
seyicoded <opadonuseyi01@gmail.com>
2023-09-17 11:26:15 +0100

    update

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  BASE_URL: "http://localhost:8001/",
-  // BASE_URL: "https://xpiracy-app-6pvpw.ondigitalocean.app/",
+  // BASE_URL: "http://localhost:8001/",
+  BASE_URL: "https://xpiracy-app-6pvpw.ondigitalocean.app/",
   COINBASE_API_KEY: "c3d44e22-6249-485a-9a78-005be8111c3d",
 };
